7a1d56fac3976187e7d9c8531256a2e48e59f4ec
AakSin <as15037@nyu.edu>
2025-05-11 19:19:20 +0400

    updated resume

--- src/pages/components/About.jsx
+++ src/pages/components/About.jsx
@@ -23,7 +23,7 @@ export default function About() {
           and fractured.
         </p>
         <a
-          href="./Aakarsh_Singh_Resume_230325.pdf"
+          href="./Aakarsh_Singh_Resume.pdf"
           target="_blank"
           rel="noreferrer"
           className="pr-2 font-bold underline underline-offset-4"
